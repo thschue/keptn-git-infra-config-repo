cb34f21d59c507adef7a6f878a91c0287f22ceb4
PodTatoHead - Infrastructure <demo@keptn.sh>
2021-09-13 06:21:05 +0200

    Update service infrastructure to version 0.0.59

--- base/infrastructure/terraform/terraform.sh
+++ base/infrastructure/terraform/terraform.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-export TF_DATA_DIR=/tmp/.terraform/infra
-
 export TF_WORKING_DIR=/keptn/terraform
 export GOOGLE_APPLICATION_CREDENTIALS=${TF_WORKING_DIR}/account.json
 export TF_VAR_keptn_token="${keptn-api-token}"
 echo "${GCP_SA}" > ${TF_WORKING_DIR}/account.json
 
+export TF_DATA_DIR=/tmp/.terraform/infra
 terraform -chdir=${TF_WORKING_DIR}/infra init -backend-config="bucket=${GCP_BUCKET}" -backend-config="prefix=terraform/state-${KEPTN_STAGE}"
 terraform -chdir=${TF_WORKING_DIR}/infra validate
 terraform -chdir=${TF_WORKING_DIR}/infra plan -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -out ${TF_WORKING_DIR}/infra.tfplan
@@ -17,4 +16,9 @@ export TF_DATA_DIR=/tmp/.terraform/keptn
 terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane init -backend-config="bucket=${GCP_BUCKET}" -backend-config="prefix=terraform/state-keptn-${KEPTN_STAGE}"
 terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane validate
 terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane plan -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -out ${TF_WORKING_DIR}/keptn-exec.tfplan
-# terraform -chdir=${TF_WORKING_DIR}/infra apply -input=false
+
+export TF_DATA_DIR=/tmp/.terraform/infra
+TF_DATA_DIR=/tmp/.terraform/infra terraform -chdir=${TF_WORKING_DIR}/infra apply -input=false
+
+export TF_DATA_DIR=/tmp/.terraform/keptn
+terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane apply -input=false
