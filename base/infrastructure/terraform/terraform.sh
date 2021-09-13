37b114e2d0bed1f6cd4af69afab54f00a3e1a094
PodTatoHead - Infrastructure <demo@keptn.sh>
2021-09-13 06:51:33 +0200

    Update service infrastructure to version 0.0.62

--- base/infrastructure/terraform/terraform.sh
+++ base/infrastructure/terraform/terraform.sh
@@ -12,13 +12,13 @@ terraform -chdir=${TF_WORKING_DIR}/infra init -backend-config="bucket=${GCP_BUCK
 terraform -chdir=${TF_WORKING_DIR}/infra validate
 terraform -chdir=${TF_WORKING_DIR}/infra plan -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -out ${TF_WORKING_DIR}/infra.tfplan
 
-export TF_DATA_DIR=/tmp/.terraform/keptn
-terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane init -backend-config="bucket=${GCP_BUCKET}" -backend-config="prefix=terraform/state-keptn-${KEPTN_STAGE}"
-terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane validate
-terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane plan -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -out ${TF_WORKING_DIR}/keptn-exec.tfplan
+# export TF_DATA_DIR=/tmp/.terraform/keptn
+# terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane init -backend-config="bucket=${GCP_BUCKET}" -backend-config="prefix=terraform/state-keptn-${KEPTN_STAGE}"
+# terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane validate
+# terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane plan -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -out ${TF_WORKING_DIR}/keptn-exec.tfplan
 
 export TF_DATA_DIR=/tmp/.terraform/infra
 terraform -chdir=${TF_WORKING_DIR}/infra apply -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -input=false -auto-approve
 
-export TF_DATA_DIR=/tmp/.terraform/keptn
-terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane apply -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -input=false -auto-approve
+# export TF_DATA_DIR=/tmp/.terraform/keptn
+# terraform -chdir=${TF_WORKING_DIR}/keptn_execution_plane apply -var-file=${TF_WORKING_DIR}/terraform.tfvars -var="gke_project=${GCP_PROJECT}" -var="gke_region=${GCP_REGION}" -input=false -auto-approve
