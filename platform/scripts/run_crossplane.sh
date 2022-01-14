1434743d652f589236bd366e7ae0b8a38ac72a01
Keptn Promotion Service <noreply@keptn.sh>
2022-01-14 09:26:19 +0000

    Updated to version 0.0.42

--- platform/scripts/run_crossplane.sh
+++ platform/scripts/run_crossplane.sh
@@ -1,4 +1,4 @@
 #/bin/ash
 /kubectl apply -k /keptn/crossplane/.
-/kubectl wait --for=condition=synced cluster/${KEPTN_STAGE}-cluster --timeout=600s
-/kubectl wait --for=condition=synced nodepool/${KEPTN_STAGE}-nodepool --timeout=600s
+/kubectl wait --for=condition=ready cluster/${KEPTN_STAGE}-cluster --timeout=600s
+/kubectl wait --for=condition=ready nodepool/${KEPTN_STAGE}-nodepool --timeout=600s
