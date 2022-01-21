2db9b1a8d0ac9037065ab986758023dd76077dd2
Platform - Infrastructure <demo@keptn.sh>
2022-01-21 07:41:39 +0100

    Update service platform to version 0.0.45

--- base/platform/scripts/run_crossplane.sh
+++ base/platform/scripts/run_crossplane.sh
@@ -1,4 +1,7 @@
 #/bin/ash
+
+set -e
+
 /kubectl apply -k /keptn/crossplane/.
 /kubectl wait --for=condition=Ready cluster/${KEPTN_STAGE}-cluster --timeout=600s
 /kubectl wait --for=condition=Ready nodepool/${KEPTN_STAGE}-nodepool --timeout=600s
