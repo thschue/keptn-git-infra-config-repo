3a6a51159fbdca01f1d7232bc5ddd09df66157b0
Platform - Infrastructure <demo@keptn.sh>
2022-01-21 12:56:54 +0100

    Update service platform to version 0.0.78

--- base/platform/scripts/run_crossplane.sh
+++ base/platform/scripts/run_crossplane.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-/kubectl create namespace argo-rollouts | true
-/kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
-
 /kubectl apply -k /keptn/crossplane/.
 /kubectl wait --for=condition=Ready cluster/${KEPTN_STAGE}-fosdem-cluster --timeout=600s
 /kubectl wait --for=condition=Ready nodepool/${KEPTN_STAGE}-fosdem-nodepool --timeout=600s
